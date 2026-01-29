82844af56a669fd308bcd146db99822ecee0ee53
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-29 15:57:57 +0530

    origin and crediential persmission

--- worksana-backend/index.js
+++ worksana-backend/index.js
@@ -11,7 +11,10 @@ import reportRouter from './routes/report.route.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
