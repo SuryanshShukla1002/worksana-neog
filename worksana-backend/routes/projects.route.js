bf8d5062edb4b1af9c79af28f6e24e04310f6b45
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-02-05 01:12:54 +0530

    id added

--- worksana-backend/routes/projects.route.js
+++ worksana-backend/routes/projects.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllProject, newProject } from '../controllers/projects.controller.js';
+import { getAllProject, getProjectById, newProject } from '../controllers/projects.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post("/projects", verifyToken, newProject);
 router.get("/projects", verifyToken, getAllProject);
+router.get("/getProject/:projectId", verifyToken, getProjectById);
 
 export default router;
