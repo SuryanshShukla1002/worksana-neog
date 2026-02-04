63ef14f2ea43ab02c82c6ddcd64d2036a25eef5e
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-02-05 01:12:14 +0530

    Fetch tasks by project with populated relations

--- worksana-backend/controllers/projects.controller.js
+++ worksana-backend/controllers/projects.controller.js
@@ -1,4 +1,5 @@
 import Project from '../models/project.models.js';
+import Task from '../models/task.model.js';
 
 export const newProject = async (req, res, next) => {
     try {
@@ -13,6 +14,18 @@ export const getAllProject = async (req, res, next) => {
     try {
         const allProject = await Project.find();
         res.status(200).json(allProject);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getProjectById = async (req, res, next) => {
+    try {
+        const { projectId } = req.params;
+        const getEachProject = await Task.find({ project: projectId }).populate("project", "name description")
+            .populate("team", "name description")
+            .populate("owners", "name email");
+        res.status(200).json(getEachProject);
     } catch (error) {
         next(error);
     }
